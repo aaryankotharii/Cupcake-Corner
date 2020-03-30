025d555ac2b199eaa66f82032c8d7171a21403fa
aaryankotharii <aaryan.kothari@gmail.com>
2020-03-30 18:37:41 +0530

    using disabled func

--- Cupcake Corner/ContentView.swift
+++ Cupcake Corner/ContentView.swift
@@ -72,8 +72,32 @@ struct ContentView: View {
     }
 }
 
+
+struct ContentView2: View{
+    @State var username = ""
+    @State var email = ""
+    
+    var diableForm: Bool {
+        username.count < 5 || email.count < 5
+    }
+    var body: some View{
+        Form{
+            Section{
+                TextField("Username", text: $username)
+                TextField("Email",text: $email)
+            }
+            
+            Section{
+                Button("Create account"){
+                    print("Creating account")
+                }
+            }.disabled(diableForm)
+        }
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView2()
     }
 }
